ea2072a6996ab19f5bf9113889eb77a50f40ffd4
paportefiax <paportefaix@gmail.com>
2019-09-10 14:44:21 +0200

    Spring JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -7,20 +7,23 @@ import io.pivotal.pal.tracker.repository.JdbcTimeEntryRepository;
 import io.pivotal.pal.tracker.repository.TimeEntryRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class PalTrackerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PalTrackerApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(PalTrackerApplication.class, args);
+        DataSource bean = context.getBean(DataSource.class);
+        System.out.println(bean);
     }
 
     @Bean
-    TimeEntryRepository inMemoRepo(){
-        MysqlDataSource dataSource = new MysqlDataSource();
-        dataSource.setUrl(System.getenv("SPRING_DATASOURCE_URL"));
+    TimeEntryRepository timeEntryRepository(DataSource dataSource) {
         return new JdbcTimeEntryRepository(dataSource);
     }
 }
